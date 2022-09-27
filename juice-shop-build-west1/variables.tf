d1586cd869a8f51916dd33d4b0d5b45edc619960
YMA1979 <49565607+YMA1979@users.noreply.github.com>
2022-09-27 17:09:14 +0100

    Update variables.tf

--- juice-shop-build-west1/variables.tf
+++ juice-shop-build-west1/variables.tf
@@ -1,9 +1,9 @@
 variable "prefix" {
   description = "prefix for resources created"
-  default     = "OB1"
+  default     = "yaz"
 }
 
 variable "ssh_key_name" {
   description = "prefix for resources created"
-  default     = "OB1west1"
+  default     = "yazwest1"
 }
